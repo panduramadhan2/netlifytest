a17c74de91463ab608a876ac629d49f188de93d9
panduramadhan2 <panduramadhan2@gmail.com>
2023-03-24 21:33:42 +0700

    ubah bank bri saja

--- src/api.js
+++ src/api.js
@@ -21,7 +21,7 @@ router.post("/test", (req, res) => {
     rek: "1",
     dob: "2000-04-29",
     balance: 7777777777,
-    bank: "bri -56517617235612",
+    bank: "bri",
     accountNumber: 123412341234,
     roles: [{ id: 1, name: "ROLE_USER" }],
   });
